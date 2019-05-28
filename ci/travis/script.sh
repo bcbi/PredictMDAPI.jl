dcd1ca430b3bbcd9c6ad2472bd3e97727679dcab
Dilum Aluthge <dilum@aluthge.com>
2019-05-28 04:40:22 -0400

    Improve tests

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -2,26 +2,29 @@
 
 set -ev
 
-julia --check-bounds=yes --color=yes -e '
+echo "COMPILED_MODULES=$COMPILED_MODULES"
+export JULIA_FLAGS="--check-bounds=yes --code-coverage=all --color=yes --compiled-modules=$COMPILED_MODULES --inline=no"
+echo "JULIA_FLAGS=$JULIA_FLAGS"
+
+julia $JULIA_FLAGS -e '
     import Pkg;
     Pkg.build("PredictMDAPI");
     '
 
-julia --check-bounds=yes --color=yes -e '
-    import PredictMDAPI;
-    '
-
-julia --check-bounds=yes --color=yes -e '
+julia $JULIA_FLAGS -e '
     import Pkg;
     Pkg.test("PredictMDAPI"; coverage=true);
     '
 
-julia --check-bounds=yes --color=yes -e '
+cat Project.toml
+cat Manifest.toml
+
+julia $JULIA_FLAGS -e '
     import Pkg;
     try Pkg.add("Coverage") catch end;
     '
 
-julia --check-bounds=yes --color=yes -e '
+julia $JULIA_FLAGS -e '
     import Coverage;
     import PredictMDAPI;
     cd(joinpath(dirname(pathof(PredictMDAPI)), "..",));
@@ -29,7 +32,6 @@ julia --check-bounds=yes --color=yes -e '
     '
 
 cat Project.toml
-
 cat Manifest.toml
 
 ##### End of file
