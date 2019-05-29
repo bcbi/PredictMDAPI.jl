92c69df74f26e77182cdf837a5d9308dabc11dbe
Dilum Aluthge <dilum@aluthge.com>
2019-05-29 03:45:45 -0400

    Progress commit

--- src/PredictMDAPI.jl
+++ src/PredictMDAPI.jl
@@ -1,7 +1,12 @@
 module PredictMDAPI
 
 include("types.jl")
+
 include("traits.jl")
-include("functions.jl")
+
+include("functions_fittables.jl")
+include("functions_metrics.jl")
+include("functions_plots.jl")
+include("functions_saveload.jl")
 
 end # end module PredictMDAPI
